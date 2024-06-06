37c0f10e877a5362c19f6dc74e87a28fc82ac443
tgalpha <tanalpha.zhy@gmail.com>
2024-06-06 18:19:49 +0800

    simple test

--- test/waapi_client_dart_test.dart
+++ test/waapi_client_dart_test.dart
@@ -1,18 +1,21 @@
+import 'package:logger/logger.dart';
 import 'package:waapi_client_dart/waapi_client_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final logger = Logger();
   group('A group of tests', () {
     final client = WaapiClient();
 
     setUp(() async {
+      logger.w('Must open Wwise Authoring to run tests.');
       // Additional setup goes here.
       await client.connect();
     });
 
     test('waapi function call', () async {
       final res = await client.getWwiseInfo();
-      // expect();
+      expect(res, isNotNull);
     });
   });
 }
