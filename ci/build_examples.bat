5b9fde866a7695c7db33ed50e7eaafffaf9ee1ff
tgalpha <tanalpha.zhy@gmail.com>
2025-07-22 14:38:17 +0800

    support dart 3.0

--- ci/build_examples.bat
+++ ci/build_examples.bat
@@ -12,14 +12,14 @@ if NOT EXIST build (
 
 
 echo building waapi_client_dart_example
-call dart compile exe example/waapi_client_dart_example.dart -o build/waapi_client_dart_example.exe
+call fvm dart compile exe example/waapi_client_dart_example.dart -o build/waapi_client_dart_example.exe
 set result=%errorlevel%
 if NOT %result% == 0 (
 	goto :fail
 )
 
 echo building post_event_repeatly
-call dart compile exe example/post_event_repeatly.dart -o build/post_event_repeatly.exe
+call fvm dart compile exe example/post_event_repeatly.dart -o build/post_event_repeatly.exe
 set result=%errorlevel%
 if NOT %result% == 0 (
 	goto :fail
